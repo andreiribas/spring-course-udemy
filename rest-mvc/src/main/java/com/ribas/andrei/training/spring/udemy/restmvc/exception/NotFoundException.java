3cea591b3ce948e697277ff14dfa4a8295f6fe31
Andrei Gonçalves Ribas <andrei.g.ribas@gmail.com>
2026-02-23 22:34:40 +0100

    feat: Adding the annotation to the exception, to use it, remove the controller advice

--- rest-mvc/src/main/java/com/ribas/andrei/training/spring/udemy/restmvc/exception/NotFoundException.java
+++ rest-mvc/src/main/java/com/ribas/andrei/training/spring/udemy/restmvc/exception/NotFoundException.java
@@ -1,5 +1,9 @@
 package com.ribas.andrei.training.spring.udemy.restmvc.exception;
 
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.ResponseStatus;
+
+//@ResponseStatus(value = HttpStatus.NOT_FOUND, reason = "Not Found")
 public class NotFoundException extends RuntimeException {
     public NotFoundException(String message) {
         super(message);
