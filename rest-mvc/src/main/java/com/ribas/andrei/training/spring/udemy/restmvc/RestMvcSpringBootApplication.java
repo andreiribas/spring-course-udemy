8c61a4d52d1a0bdeb17219dff344378439dfe2e8
Andrei Gonçalves Ribas <andrei.g.ribas@gmail.com>
2026-02-27 23:53:53 +0100

    refactor: Adding annotations to the spring boot main class

--- rest-mvc/src/main/java/com/ribas/andrei/training/spring/udemy/restmvc/RestMvcSpringBootApplication.java
+++ rest-mvc/src/main/java/com/ribas/andrei/training/spring/udemy/restmvc/RestMvcSpringBootApplication.java
@@ -2,8 +2,12 @@ package com.ribas.andrei.training.spring.udemy.restmvc;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.persistence.autoconfigure.EntityScan;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
-@SpringBootApplication
+@SpringBootApplication(scanBasePackages = "com.ribas.andrei.training.spring.udemy")
+@EnableJpaRepositories(basePackages = "com.ribas.andrei.training.spring.udemy.domain.repository")
+@EntityScan(basePackages = "com.ribas.andrei.training.spring.udemy.domain.model")
 public class RestMvcSpringBootApplication {
     static void main(String[] args) {
         SpringApplication.run(RestMvcSpringBootApplication.class, args);
